ee9656883e8b0ea4a582cc76a651e930e8517d61
IDGS-802-23002541 <83322@alumnos.utleon.edu.mx>
2026-03-22 18:20:01 -0600

    archivo junto el merge anterior

--- proveedores/__init__.py
+++ proveedores/__init__.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-from flask import Blueprint, render_template, request, redirect, url_for
-
-proveedores_bp = Blueprint(
-    'proveedores',
-    __name__,
-    template_folder='templates'
-)
-
-@proveedores_bp.route("/proveedores", methods=["GET", "POST"])
-def auth():
-    return render_template("proveedores/proveedores.html")
-=======
 from flask import Blueprint
 
 proveedores = Blueprint(
@@ -20,4 +7,3 @@ proveedores = Blueprint(
 )
 
 from .routes import proveedores
->>>>>>> 1fd909cfe1d16c14c53fdd72a8e14ae08acb739a
