4a80741d2c913424b4ef117c361b66f13d009774
IDGS-802-23002541 <83322@alumnos.utleon.edu.mx>
2026-03-27 03:53:19 -0600

    Se agrega el modelo compra

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-import datetime 
+import datetime
 db = SQLAlchemy()
 
 class Proveedor(db.Model):
@@ -16,3 +16,15 @@ class Proveedor(db.Model):
 
     def __repr__(self):
         return f'<Proveedor {self.razonSocial}>'
+
+class Compra(db.Model):
+    __tablename__='compra'
+
+    id=db.Column(db.Integer, primary_key=True)
+    factura=db.Column(db.String(50))
+    fechaCompra=db.Column(db.Date, default=datetime.datetime.now)
+    idProveedor=db.Column(db.Integer)
+    idUsuario=db.Column(db.Integer)
+
+    proveedor=db.relationship('Proveedor', back_populates='compra')
+    
