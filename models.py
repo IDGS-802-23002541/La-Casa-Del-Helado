bc9f2f478d593550381fb6e8b9b02ecde040ea22
Vanesarea <vanesareamunoz.12@gmail.com>
2026-03-30 23:35:48 -0600

    Modulo solicitudes a produccion terminado

--- models.py
+++ models.py
@@ -11,22 +11,20 @@ class Proveedor(db.Model):
     telefono = db.Column(db.String(15), nullable=False)
     direccion = db.Column(db.String(200), nullable=False)
     estatus = db.Column(db.String(20), default='Activo')
-
     # compras = db.relationship('Compra', backref='proveedor', lazy=True)
     def __repr__(self):
         return f'<Proveedor {self.razonSocial}>'
     
 class SolicitudProduccion(db.Model):
-    __tablename__ = 'solicitud_produccion'
-    
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    producto = db.Column(db.String(100), nullable=False)
-    cantidad = db.Column(db.Integer, nullable=False)
-    fecha_solicitud = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    estatus = db.Column(db.String(20), default='Pendiente')
-
+    __tablename__ = 'solicitudproduccion'
+    id = db.Column(db.Integer, primary_key=True)
+    fecha = db.Column(db.Date, nullable=False)
+    estatus = db.Column(db.String(20), nullable=False)
+    idProducto = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    cantidad = db.Column(db.Numeric(10, 2), nullable=False)
+    producto = db.relationship('Producto', backref='solicitudes_produccion')
     def __repr__(self):
-        return f'<SolicitudProduccion {self.producto} - {self.cantidad}>'
+        return f'<Solicitud {self.id} - Producto {self.idProducto}>'
 
 class Producto(db.Model):
     __tablename__ = 'producto'
