8c60fd593c4be883439ad1ed4ebf5fbadbffaf94
IDGS-802-23002541 <83322@alumnos.utleon.edu.mx>
2026-04-15 14:43:38 -0600

    correciones coherencia db

---
+++ migrations/versions/a50534603a70_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: a50534603a70
+Revises: a0da59438d8a, a36b0e83266c
+Create Date: 2026-04-15 14:35:24.118562
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a50534603a70'
+down_revision = ('a0da59438d8a', 'a36b0e83266c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
